a08294edbe700b659efcd69a19be1fa9daab4f2b
sofi97-code <125391720+sofi97-code@users.noreply.github.com>
2023-02-20 00:02:12 +0100

    Add files via upload

---
+++ oddWhile.js
@@ -0,0 +1,6 @@
+let number = 1;
+while ( number = 1) 
+{
+    console.log(number);
+    number += 2;
+}
---
+++ oneToTen.js
@@ -0,0 +1,6 @@
+var val = 0
+
+for (var i = 0++; i <= 10; i ++ ) {
+    val = i;
+    console.log(val);
+}
